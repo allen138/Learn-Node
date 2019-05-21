326b0d76f7a7ad033d44f55204c4db3c6a7ba0dc
allen138 <amdev1038@gmail.com>
2019-05-21 14:49:37 -0700

    modules 20-38

--- Big-Als-Resturaunt/controllers/storeController.js
+++ Big-Als-Resturaunt/controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
@@ -44,6 +45,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
+    req.body.author = req.user._id;
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
     res.redirect(`/store/${store.slug}`);
@@ -55,10 +57,17 @@ exports.getStores = async (req, res) => {
     res.render('stores', { title:"Stores", stores })
 };
 
+const confirmOwner = (store, user) => {
+    if(!store.author.equals(user._id)) {
+        throw Error('You must be the owner of the store to edit');
+    }
+}
+
 exports.editStore = async (req, res) => {
     // 1. find the store givin the ID
     const store = await Store.findOne({ _id: req.params.id });
     // 2. confirm they are the owner of the store
+    confirmOwner(store, req.user);
     // 3. render out the edit form so the user can update their store
     res.render('editStore', { title: `Edit ${store.name}`, store });
 };
@@ -74,8 +83,77 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res) => {
-    const store = await Store.findOne({slug: req.params.slug});
+    const store = await Store.findOne({slug: req.params.slug}).populate('author reviews');
     if(!store) return next();
     res.render('store', {store, title: store.name});
-}
+};
 
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({tags: tagQuery});
+    const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+    res.render('tag', {tags, stores, tag, title: 'Tags'});
+};
+
+exports.searchStores = async (req, res) => {
+    const stores = await Store
+    .find({
+        $text: {
+            $search: req.query.q,    
+        }
+    }, {
+        score: { $meta: 'textScore' }
+    })
+    .sort({
+        score: { $meta: 'textScore' }
+    })
+    //limit to only 5 results
+    .limit(5);
+    res.json(stores);
+};
+
+exports.mapStores = async (req, res) => {
+    const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+    const q = {
+        location: {
+          $near: {
+            $geometry: {
+              type: 'Point',
+              coordinates
+            },
+            $maxDistance: 10000 // 10km
+          }
+        }
+      };
+    const stores = await Store.find(q).select('slug name description location photo').limit(10);
+    res.json(stores);
+};
+
+exports.mapPage = (req, res) => {
+    res.render('map', {title: "Map"});
+};
+
+exports.heartStore = async (req, res) => {
+    const hearts = req.user.hearts.map(obj => obj.toString());
+    const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+    const user = await User
+        .findByIdAndUpdate(req.user._id, 
+            {[operator]: { hearts: req.params.id}}, 
+            { new: true}
+        );
+    res.json(user);
+};
+
+exports.getHeartedStores = async (req, res) => {
+    const stores = await Store.find({
+        _id: { $in: req.user.hearts }
+    });
+    res.render('stores', {title: 'Hearted Stores', stores});
+};
+
+exports.getTopStores = async (req, res) => {
+    const stores = await Store.getTopStores();
+    res.render('topStores', {stores, title: 'Top Stores!'});
+}
--- Big-Als-Resturaunt/models/Store.js
+++ Big-Als-Resturaunt/models/Store.js
@@ -32,18 +32,81 @@ const storeSchema = new mongoose.Schema({
             required: 'You must supply the address'
         }
     },
-    photo: String
-
+    photo: String,
+    author: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: 'You must supply an author.'
+    }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
-storeSchema.pre('save', function(next) {
+// define our indexes
+
+storeSchema.index({
+    name: 'text',
+    description: 'text'
+});
+
+storeSchema.index({ location: '2dsphere'});
+
+storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
         next();
         return;
     }
     this.slug = slug(this.name);
-    next();
-    // TODO make sure slugs are unique. 
+    // find other stores that have similar name
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({slug: slugRegEx});
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+    next(); 
 });
+
+storeSchema.statics.getTagsList = function() {
+    return this.aggregate([
+        { $unwind: '$tags'},
+        { $group: {_id: '$tags', count: {$sum: 1} }},
+        { $sort: { count: -1 }}
+    ]);
+};
+
+storeSchema.statics.getTopStores = function() {
+    return this.aggregate([
+        // look up stores and populate review
+        { $lookup: {
+            from: 'reviews', 
+            localField: '_id', 
+            foreignField: 'store', 
+            as: 'reviews'}
+        },
+        // filter for lny items that have 2 or more reviews
+        { $match: {
+            'reviews.1': { $exists: true }
+        }},
+        // add the average review feilds
+        { $project: {
+            photo: '$$ROOT.photo',
+            name: '$$ROOT.name',
+            reviews: '$ROOT.reviews',
+            slug: '$$ROOT.slug',
+            averageRating: {$avg: '$reviews.rating'}
+        }},
+        // sort it by our new feild, heighest to lowest
+        { $sort: { averageRating: -1 }},
+        // limit to only 10
+        { $limit: 10}
+    ])
+}
+
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link?
+    localField: '_id', // which field on the store?
+    foreignField: 'store' // which field on the store?
+})
 
 module.exports = mongoose.model('Store', storeSchema);
